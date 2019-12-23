14c20698d3c4dc1d42b75b4b351dd3f91761c32b
Mishra Gaurav <gm150180107025@gmail.com>
2019-12-24 00:34:41 +0530

    + Added functions and some logging info
    + Functionality:
      "sh", "py", "java" will be added in ~/Documents/Projects/
      from now.

--- core/watchdog.py
+++ core/watchdog.py
@@ -1,13 +1,23 @@
 import logging
-import sys
 import threading
 import shutil
 from time import sleep
-from os import listdir, path, mkdir
+from os import listdir, path, mkdir, nice
 from os.path import join
 from multiprocessing import cpu_count
 import signal
 
+
+# TODO:
+#  Collective:
+#        # Modify the folder creation during file checking.
+#        # Sleep according to CPU count
+#        # if __main__ code sanitation
+#        # More detail in os.nice
+#  Next:
+#       # Add Description in selection Type for DeepRoots
+#       # Add more workouts for Affirmation
+#       # Clear Out atleast Affirmation and Gathering.
 
 class FileHandler:
 
@@ -15,8 +25,8 @@ class FileHandler:
         """
         Notes:
             Watchdog is specifically designed to keep a constant eye on a folder (currently), could work
-            under daemon mode, battery saver mode and super persistent mode ( Only on Linux ) and works
-            on default normal execution mode which takes gaps to maintain a offload on low end machines.
+            under daemon, battery saver and super persistent mode ( Only on Linux ) and works
+            on default normal execution mode which takes gaps to maintain a low search overhead on low end machines.
 
         Args:
             track: Specifies the folder that needs to be kept under constant track is kept by default at Downloads folder.
@@ -30,74 +40,29 @@ class FileHandler:
         self.selection_type = selection
         self.source_to_track = path.expanduser(track)
         self.folder_destination = ''
+        self._priority = None
 
     def tracker(self):
+        """
+        Notes: This function does initial setup of the program and then initiate the run.
+
+        Raises: Error if any.
+        """
         try:
-
-            if self.mode == 0:
-                print(cpu_count())
-                logging.info('... Working Under Normal Mode (Default) ...')
-
-            elif self.mode == 1:
-                logging.info('... Working Under Daemon Mode ...')
-
-            elif self.mode == 2:
-                logging.info('... Working Under Super Persistent Mode ...')
-
-            elif self.mode == 3:
-                logging.info('... Working Under Power Saving Mode ...')
-
-            else:
-                logging.info('No mode found switching to default Normal Mode')
+            self.GatheringInfo()
+            self.AffirmingSetting()
 
             if self.selection_type == 0:
-                # Default No Folder traversal...
-                while True:
-
-                    for filename in listdir(self.source_to_track):
-                        try:
-                            self.decider(file=str(filename))
-
-                            if self.folder_destination != '':
-                                print('Moving ', filename, 'to', self.folder_destination)
-                                shutil.move(join(self.source_to_track, filename),
-                                            join(self.folder_destination, filename))
-                                self.folder_destination = ''
-
-                        except FileNotFoundError:
-                            mkdir(self.folder_destination)
-                            continue
-
-                        except BaseException as B:
-                            print('error encountered', B)
-                            pass
-                    sleep(20)
+                self.NormalMode()
 
             elif self.selection_type == 1:
-                # Deep_roots
-                for filename in listdir(self.source_to_track):
-                    try:
-                        self.decider(file=str(filename))
-                        if path.isdir(filename):
-                            continue
-                        if self.folder_destination != '':
-                            shutil.move(join(self.source_to_track, filename), join(self.folder_destination, filename))
-                            self.folder_destination = ''
-                        print('Sleeping for 5 sec')
-                        sleep(5)
-                        print('Awaken')
+                self.DeepRootMode()
 
-                    except FileNotFoundError:
-                        mkdir(self.folder_destination)
-                        continue
-
-                    except BaseException as B:
-                        print('error encountered', B)
-                        pass
             else:
                 exit('!! No Selection Captured !!')
 
         except BaseException as B:
+            logging.info(B)
             raise B
 
     def decider(self, file):
@@ -106,6 +71,9 @@ class FileHandler:
             if ext in ["pdf", 'txt', "docx", "doc", "odt", "rtf", "xlsx", "csv", 'gif', 'wps', 'wpd', 'ppt', 'pptx',
                        'xls']:
                 self.folder_destination = path.expanduser('~/Documents/')
+                # TODO Log here
+            elif ext in ['sh', 'py', 'java']:
+                self.folder_destination = path.expanduser('~/Documents/ProjectFile/')
                 # TODO Log here
             elif ext in ['zip', 'tar', '7z']:
                 self.folder_destination = path.expanduser('~/Documents/Compressed/')
@@ -124,10 +92,108 @@ class FileHandler:
                 pass
 
         except FileNotFoundError:
-            mkdir(self.folder_destination)
+            mkdir(self.folder_destination, mode=777)
+            return True
 
         except BaseException as B:
             raise B
+
+    def DeepRootMode(self):
+        """
+            Notes:
+                #  what: this is DeepRoot selection mode which include
+                # * Deeper Folder traversal Depth [Default: Two folder].
+                # * Above Avg cpu overhead with check_call at 8 sec [ Has to check in deeper nodes ].
+                # * MultiThreading for each check_call.
+                # * considerable above priority over other process.
+        """
+
+        for filename in listdir(self.source_to_track):
+            try:
+                self.decider(file=str(filename))
+                if path.isdir(filename):
+                    continue
+                if self.folder_destination != '':
+                    shutil.move(join(self.source_to_track, filename), join(self.folder_destination, filename))
+                    self.folder_destination = ''
+                print('Sleeping for 5 sec')
+                sleep(5)
+                print('Awaken')
+
+            except FileNotFoundError:
+                mkdir(self.folder_destination)
+                continue
+
+            except BaseException as B:
+                print('error encountered', B)
+                pass
+
+    def NormalMode(self):
+        """
+            Notes:
+                #  what: this is Default selection which include
+                # * No Deeper Folder traversal.
+                # * Avg cpu overhead with check_call at 10 sec.
+                # * MultiThreading for each check_call.
+                # * considerable priority over other process.
+        """
+        while True:
+
+            for filename in listdir(self.source_to_track):
+                try:
+                    self.decider(file=str(filename))
+
+                    if self.folder_destination != '':
+                        print('Moving ', filename, 'to', self.folder_destination)
+                        shutil.move(join(self.source_to_track, filename),
+                                    join(self.folder_destination, filename))
+                        self.folder_destination = ''
+
+                except FileNotFoundError:
+                    mkdir(self.folder_destination)
+                    continue
+
+                except BaseException as B:
+                    logging.error(B)
+                    continue
+            sleep(20)
+
+    def AffirmingSetting(self):
+        """
+        Notes: This function makes final settings opted by user, all this setting are then passed and utilized during
+                file catching process.
+
+        Returns: Nothing
+
+        """
+
+        # what :- Setting the nice(Priority) value to the program [Default = 10]
+        # why :- this is done to make the code more or less persistent over the base system
+        # how :- value of niceness is decided by program according to the mode of selection.
+        nice(self._priority)
+
+    def GatheringInfo(self):
+
+        if self.mode == 0:
+            print(cpu_count())
+            logging.info('... Working Under Normal Mode (Default) ...')
+            self._priority = 10
+
+        elif self.mode == 1:
+            logging.info('... Working Under Daemon Mode ...')
+            self._priority = -15
+
+        elif self.mode == 2:
+            logging.info('... Working Under Super Persistent Mode ...')
+            self._priority = -18
+
+        elif self.mode == 3:
+            logging.info('... Working Under Power Saving Mode ...')
+            self._priority = 12
+
+        else:
+            logging.info('No mode found switching to default Normal Mode')
+            self._priority = -3
 
 
 def validateParams(s_user, s_mode, s_track):
@@ -169,9 +235,7 @@ if __name__ == '__main__':
             signal.signal(signal.SIGALRM, handler)
 
             signal.alarm(1)
-
             prompt_available = input('Would you like to go with defaults (0/1): ')
-
             signal.alarm(0)
 
         except TimeoutError as S:
